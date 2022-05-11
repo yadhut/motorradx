9f52c6dc1a699f89cd6368ed957995041cc1b7fb
Yadhu krishnan <yadhukris90@gmail.com>
2022-05-11 11:58:01 +0530

    added edit and update functionality in user_controller also created view for the corresponding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,44 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+
+    def edit
+
+        @user = User.find(params[:id])
+    end
+
+
+    def update
+        @user = User.find(params[:id])
+        
+        @user.update(params.require(:user).permit(:username, :email, :password))
+        byebug
+        if @user.errors.empty?  
+            flash[:notice] = "Your account has been updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+
+
+    end
+
+
+
     def create
+        @user = User.new(user_params)
+        if(@user.save)
+            flash(:notice, "User created successfully")
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
 
-        @user = User.new()
+    private
 
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 
 end
